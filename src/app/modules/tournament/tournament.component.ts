8840040d70fc72210ec75728c9d372c2ad636d49
Igor <igor.silva@7comm.com.br>
2021-09-07 15:16:38 -0300

    Adiantando estrutura e iteracao, TODO: por valores dinamicos baseado no array setado

--- src/app/modules/tournament/tournament.component.ts
+++ src/app/modules/tournament/tournament.component.ts
@@ -1,23 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Store } from "@ngrx/store";
+import { AppState } from "../core/store/app-state";
+import { selectTeams } from "../core/store/reducers/teams/teams.reducer";
 
 @Component({
-  selector: 'app-tournament',
-  templateUrl: './tournament.component.html',
-  styleUrls: ['./tournament.component.scss']
+  selector: "app-tournament",
+  templateUrl: "./tournament.component.html",
+  styleUrls: ["./tournament.component.scss"],
 })
 export class TournamentComponent implements OnInit {
   public tournamentName?: string;
+  public teams!: Array<any>;
 
-  constructor(public route: ActivatedRoute) { }
+  constructor(public route: ActivatedRoute, private store: Store<AppState>) {}
+
+  htmlData: Array<any> = [];
 
   ngOnInit(): void {
-    console.log('resolved: ', this.dataResolved.details);
-    this.route.params.subscribe((params: any) => this.tournamentName = params['name']);
+    console.log("resolved: ", this.dataResolved.details);
+    this.getTeams();
+    this.route.params.subscribe(
+      (params: any) => (this.tournamentName = params["name"])
+    );
   }
 
   get dataResolved(): any {
     return this.route.snapshot.data;
   }
 
+  getTeams(): void {
+    const { teams } = selectTeams(this.store);
+    console.log("teams; ", teams);
+    this.teams = teams;
+    this.setData();
+  }
+
+  verifyTeam(match: any) {
+    const homeTeam = this.teams.find((val) => val.id === match?.homeTeamId);
+    const awayTeam = this.teams.find((val) => val.id === match?.awayTeamId);
+    return { homeTeam, awayTeam };
+  }
+
+  setData() {
+    (this.dataResolved.details as Array<any>).map((match: any) => {
+      const teams = this.verifyTeam(match);
+      this.htmlData.push({ ...match, ...teams });
+    });
+  }
+
+  get data() {
+    return this.htmlData;
+  }
 }
